548a205e0a0a93ab7d7dc981e7dac5db03b0f19a
ViniMachado77 <107115421+ViniMachado77@users.noreply.github.com>
2024-08-30 15:29:01 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o país com o maior território do mundo?",
+        alternativas: [
+            {
+                texto: "a) Canadá",
+                afirmacao: [
+                    "B)Rússia"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B)Rússia",
+                afirmacao: [
+                    "B)Rússia"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual país é famoso pela criação do sushi?",
+        alternativas: [
+            {
+                texto: "a) China",
+                afirmacao: [
+                    "B)Japão",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)Japão",
+                afirmacao: [
+                    "B)Japão"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o rio mais extenso do mundo??",
+        alternativas: [
+            {
+                texto: "a) Rio Amazonas",
+                afirmacao: [
+                    "b) Rio Nilo"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) Rio Nilo",
+                afirmacao: [
+                    "b) Rio Nilo"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a capital da Austrália?",
+        alternativas: [
+            {
+                texto: "A) Canberra",
+                afirmacao: [
+                    "A) Canberra"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B)Sydney",
+                afirmacao: [
+                    "A) Canberra"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a moeda oficial do Reino Unido?",
+        alternativas: [
+            {
+                texto: "a) Euro",
+                afirmacao: [
+                    "b) Libra Esterlina"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Libra Esterlina",
+                afirmacao: [
+                    "b) Libra Esterlina"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Quem escreveu a peça Romeu e Julieta",
+        alternativas: [
+            {
+                texto: "a) William Shakespeare",
+                afirmacao: [
+                    "a) William Shakespeare"
+                ],
+            },
+            {
+                texto: "b) Charles Dickens",
+                afirmacao: [
+                    "a) William Shakespeare"
+                ],
+            },
+        ]
+    },
+];
